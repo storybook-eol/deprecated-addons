b278d763c0daf2f1b488375b8fd5816918143c8a
Norbert de Langen <ndelangen@me.com>
2020-01-29 16:38:49 +0100

    CLEANUP

--- scripts/jest-ts-babel.js
+++ scripts/jest-ts-babel.js
@@ -1,8 +1,7 @@
-const tsTransformer = require('jest-preset-angular/preprocessor');
 const babelTransformer = require('./babel-jest');
 
 module.exports.process = function transform(src, path, config, transformOptions) {
-  const tsResult = tsTransformer.process(src, path, config, transformOptions);
+  const tsResult = src;
   const jsPath = path.replace('.ts', '.js');
   const source = typeof tsResult === 'string' ? tsResult : tsResult.code;
 
